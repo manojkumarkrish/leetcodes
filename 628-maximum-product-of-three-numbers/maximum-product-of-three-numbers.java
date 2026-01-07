7452a3840ed5297caaf7801a51c1cae951f342e6
manojkumarkrish <manojjkumar231@gmail.com>
2026-01-07 19:07:55 +0530

    Time: 15 ms (77.84%) | Memory: 48.4 MB (12.84%) - LeetSync

---
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maximumProduct(int[] nums) 
+    {
+        Arrays.sort(nums);
+        int l=nums.length;
+        int max1=nums[l-1]*nums[l-2]*nums[l-3];
+        int max2=nums[0]*nums[1]*nums[l-1];
+        return Math.max(max1,max2);
+    }
+}
